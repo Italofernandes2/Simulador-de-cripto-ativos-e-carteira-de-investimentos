9eded8de83a5645dc22d5798ca4be910c13f08e2
Italofernandes2 <fitalodi@gmail.com>
2026-06-01 21:37:07 -0300

    Add files via upload
    Adicionamos funcoes variacao de precos, ver mercado, ver carteira de moedas

--- main.py
+++ main.py
@@ -1,19 +1,40 @@
 """
 Projeto avaliativo P2 - Simulador de criptos ativos e carteira de investimento 
-- Membros do grupo: Italo Fernandes, Gabriel Aralli, e Henry
+- Membros do grupo: Italo Fernandes, Gabriel Aralli, e Henry Campos
 """
 #Biblioteca random para trazer aleatoriedade para o preco da cripto
 import random
 
 # Cenario inicial
 saldo_reais = 10000.00
+
 criptos = {
-    "BTC" : {"Nome": "Bitocin", "Preco": 300000.00},
-    "ETH" : {"Nome": "Ethereum", "Preco": 15000.00},
-    "DOGE" : {"Nome": "Dogecoin", "Preco": 1.20},
+    "BTC" : {"Nome": "Bitocin", "Preco": 350000.00},
+    "ETH" : {"Nome": "Ethereum", "Preco": 10000.00},
+    "DOGE" : {"Nome": "Dogecoin", "Preco": 1.00},
 }
+
 carteira = {
     "BTC": 0.00,
     "ETH": 0.00,
     "DOGE": 0.00
 }
+"""
+=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-
+"""
+#Funcoes do ciclo do mercado
+def variar_valores():
+    for abreviacao in criptos:
+        variacao = random.uniform(-0.1, 0.1) #O valor vai variar de -10% a 10%
+        criptos[abreviacao]["Preco"] = criptos[abreviacao]["Preco"]  * (1 + variacao) 
+
+def ver_mercado():
+    for abreviacao in criptos:
+        nome = criptos[abreviacao]["Nome"]
+        preco = criptos[abreviacao]["Preco"]
+        print(f"{abreviacao} ({Nome}): R$ {preco}")
+        
+def ver_carteira_de_moedas():
+    print(carteira)
+   
+def comprar(): 
